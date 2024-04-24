9af42b53b242ab9cbadb1c8958fa41d32d527a13
fHACKrenn <git.fachren.hub@gmail.com>
2024-04-24 17:10:26 +0700

    Add screenshot for README.md

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:ets/src/pages/movie_details.dart';
-import 'package:ets/src/pages/movies_list.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +8,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Movie App',
+      debugShowCheckedModeBanner: false,
+      title: 'Siksa Kubur',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.grey[900], // Dark blue background color
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MovieList(),
+      home: ImageDescriptionPage(),
+    );
+  }
+}
+
+class ImageDescriptionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Siksa Kubur'),
+      ),
+      body: Container(
+        color: Colors.grey[900], // Dark blue background color
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              'https://www.voicemagz.com/wp-content/uploads/2024/02/Film-Siksa-Kubur-Poster.jpg', // Image path
+              width: 200,
+              height: 200,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Siksa Kubur (2024)',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Grave Torture (Indonesian: Siksa Kubur) is a 2024 Indonesian psychological horror film written and directed by Joko Anwar. The film is based on Anwar\'s 2012 short film of the same name. The film stars Faradina Mufti, Reza Rahadian, Christine Hakim and Slamet Rahardjo.',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
